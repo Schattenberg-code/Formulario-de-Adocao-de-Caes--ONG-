6a18f8b5f65d8c62b87f0da552eeb3718fcc7d7d
schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-03-29 16:54:26 -0300

    add - corrigi erros com o botão submit, fiz detalhes visuais e consertei as mensagens de erro.

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ let form = document.getElementById('formulario');
 
 form.addEventListener('submit', function(e){
 
-    e.defaultPrevented();
+    console.log("funcionou o botão");
+
+    e.preventDefault();
 
     let valido= true;
 
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
     let cpf = document.getElementById('cpf').value;
     let idade = document.getElementById('idade').value;
     let cidade = document.getElementById('cidade').value;
-    let moradia = document.getElementsByName('moradia').value;
+    let moradia = document.querySelector('input[name="moradia"]:checked');
     let quintal = document.getElementsByName('quintal').value;
     let pet = document.getElementsByName('pet').value;
     let tempo = document.getElementById('tempo').value;
     let motivo = document.getElementById('motivo').value;
-    let termos = document.getElementById('aceito').value;
+    let termos = document.getElementById('termo').checked;
 
     document.getElementById('erroNome').textContent = "";
     document.getElementById('erroEmail').textContent = "";
@@ -26,35 +29,40 @@ form.addEventListener('submit', function(e){
     document.getElementById('erroTempo').textContent = "";
     document.getElementById('erroMotivo').textContent = "";
 
-    if(nome.length < 3){
-        document.getElementById('erroNome').textContent = "Seu nome precisa ter mais de 3 caracteres";
+    if(cidade.length < 1){
+        document.getElementById('erroCidade').textContent = "Insira um nome de cidade."
         valido = false;
     }
 
-    if(!email.include('@')){
-        document.getElementById('erroEmail').textContent = "Seu email precisa conter um '@'";
-        value = false;
+    if(nome.length < 3){
+        document.getElementById('erroNome').textContent = "Seu nome precisa ter mais de 3 caracteres.";
+        valido = false;
+    }
+
+    if(telefone.length < 8){
+        document.getElementById('erroTelefone').textContent = "insira um numero valido.";
+        valido = false;
+    }
+
+    if(!email.includes('@')){
+        document.getElementById('erroEmail').textContent = "Seu email precisa conter um '@'.";
+        valido = false;
     }
 
     if(cpf.length < 14){
-        document.getElementById('erroCpf').textContent = "Seu Cpf é invalido";
-        value = false;
+        document.getElementById('erroCpf').textContent = "Seu Cpf é invalido.";
+        valido = false;
     }
 
     if(idade >120 || idade < 18){
-        document.getElementById('erroIdade').textContent = "Insira um idade valida";
+        document.getElementById('erroIdade').textContent = "Insira um idade valida.";
         valido = false;
     }
 
     if(valido){
-        document.getElementById
+        alert("Formulário enviado com sucesso!");
+        form.submit();
     }
-
-
-
-
-
-
 
 })
 
