9719ef21b90c2f7ec321314e301857d5b783e619
schattenberg-code <lucas_schattenberg@estudante.sesisenai.org.br>
2026-03-30 21:22:54 -0300

    add - adicionei mais condições e resolvi problemas envolvendo os if

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let form = document.getElementById('formulario');
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function (e) {
 
     console.log("funcionou o botão");
 
     e.preventDefault();
+
+
 
     const justifica = document.querySelector("#justificativa");
     justifica.style.display = "none";
@@ -13,8 +15,15 @@ form.addEventListener('submit', function(e){
     const quintalSeguro = document.querySelector('#quintalSeguro');
     quintalSeguro.style.display = "none";
     const erroTempos = document.getElementById('erroTempo');
+    const naoPet = document.getElementById('erroPet');
+    const possuirQuintal = document.querySelector("#possuirQuintal");
+    possuirQuintal.style.display = "block";
 
-    let valido= true;
+
+
+    let valido = true;
+
+
 
     let nome = document.getElementById('nome').value;
     let email = document.getElementById('email').value;
@@ -26,7 +35,7 @@ form.addEventListener('submit', function(e){
     let permite = document.querySelector('input[name="permite"]:checked');
     let quintal = document.querySelector('input[name="quintal"]:checked');
     let seguro = document.querySelector('input[name="seguro"]:checked');
-    let pet = document.getElementsByName('pet').value;
+    let pet = document.querySelector('input[name="pet"]:checked');
     let tempo = Number(document.getElementById('tempo').value);
     let motivo = document.getElementById('motivo').value;
     let termo = document.getElementById('termo').checked;
@@ -40,87 +49,146 @@ form.addEventListener('submit', function(e){
     document.getElementById('erroMotivo').textContent = "";
     document.getElementById('erroTermo').textContent = "";
     document.getElementById('erroPet').textContent = "";
-    document.getElementById('erroMoradia').textContent ="";
+    document.getElementById('erroMoradia').textContent = "";
     document.getElementById('erroPermite').textContent = "";
     document.getElementById('erroSeguro').textContent = "";
 
-    if(!termo){
+
+
+    //------------------------------validações simples----------------------------------//
+
+    let cpfvalidados = ['11111111111111', '23459837261498'];
+    let motivosinvalidos = ['porque sim', 'porque eu quero', 'quero', 'cebola']
+
+
+    if (pet && pet.value === "naoTeve") {
+        naoPet.textContent = "Você podera receber apoio da ONG!";
+        naoPet.style.color = "black";
+    }
+
+    if (!document.getElementById('tempo').value) {
+        erroTempos.textContent = "Insira um tempo";
+        valido = false;
+    }
+
+    if (!pet) {
+        naoPet.textContent = "Informe se voce ja teve um pet."
+        valido = false;
+    }
+
+    if (!quintal) {
+        document.getElementById('erroQuintal').textContent = "selecione se voce tem quintal ou não."
+        valido = false;
+    }
+
+    if (!moradia) {
+        document.getElementById('erroMoradia').textContent = "Selecione uma moradia.";
+        valido = false;
+    }
+
+    if (!termo) {
         document.getElementById('erroTermo').textContent = "Você precisa aceitar os termos."
         valido = false;
     }
 
-    if(cidade.length < 1){
+    if (cidade.length < 1) {
         document.getElementById('erroCidade').textContent = "Insira um nome de cidade."
         valido = false;
     }
 
-    if(nome.length < 3){
+    if (nome.length < 3) {
         document.getElementById('erroNome').textContent = "Seu nome precisa ter mais de 3 caracteres.";
         valido = false;
     }
 
-    if(telefone.length < 8){
+    if (telefone.length < 8) {
         document.getElementById('erroTelefone').textContent = "insira um numero valido.";
         valido = false;
     }
 
-    if(!email.includes('@')){
+    if (!email.includes('@')) {
         document.getElementById('erroEmail').textContent = "Seu email precisa conter um '@'.";
         valido = false;
     }
 
-    if(cpf.length < 14){
+    if (cpfvalidados.includes(cpf)) {
+        document.getElementById('erroCpf').textContent = "esse CPF já esta cadastrado.";
+    } else if (cpf.length < 14) {
         document.getElementById('erroCpf').textContent = "Seu Cpf é invalido.";
         valido = false;
     }
 
-    if(idade >120 || idade < 18){
+    if (idade > 120 || idade < 18) {
         document.getElementById('erroIdade').textContent = "Insira um idade valida.";
         valido = false;
     }
 
-    if(tempo > 8){
-        erroTempos.textContent = "Você precisa de uma justificativa para esse tempo";
+
+    //------------------------validações complexas-----------------------------//
+
+    
+    if (!motivo) {
+        document.getElementById('erroMotivo').textContent = "Adicione o motivo da adoção."
+    }else{
+        if (motivosinvalidos.includes(motivo.toLowerCase())) {
+                document.getElementById('erroMotivo').textContent = "meu deus cara pelamordedeus escreve algo certo";
+        }
+    }
+    
+
+    if (tempo > 8) {
+        erroTempos.textContent = "Você precisa de uma justificativa para esse tempo.";
         justifica.style.display = "block";
         justifica.style.backgroundColor = "rgb(255, 184, 157)";
         valido = false
-        if(inputjustifica.length > 0){
+        if (inputjustifica.length > 0) {
             erroTempos.textContent = "";
             justifica.style.backgroundColor = "rgb(255, 255, 255)";
         }
+    } else if (document.getElementById('tempo').value && tempo >= 0 && tempo <= 8) {
+        erroTempos.textContent = "";
     }
 
-    if(quintal.value === "naoPossui"){
+
+    
+
+
+    if (quintal && quintal.value === "possui") {
         document.getElementById('erroQuintal').textContent = "Seu quintal é seguro?";
         quintalSeguro.style.display = "block";
-        if(seguro.value === "seguro"){
+        valido = false;
+        if (seguro && seguro.value === "seguro") {
             document.getElementById('erroQuintal').textContent = "";
             document.getElementById('erroSeguro').textContent = "";
-        } else {
-            document.getElementById('erroQuintal').textContent = "";
-            document.getElementById('erroSeguro').textContent = "problema voce não pdoe ter animais!!";
-            valido = false
-        }
+            quintalSeguro.style.backgroundColor = "white";
+            valido = true;
+            } else {
+                document.getElementById('erroQuintal').textContent = "";
+                document.getElementById('erroSeguro').textContent = "problema voce não pdoe ter animais!!";
+                quintalSeguro.style.backgroundColor = "rgb(255, 184, 157)";
+                valido = false
+            }
+    }else{
+        document.getElementById('erroQuintal').textContent = "";
     }
 
-    if(!moradia){
-    document.getElementById('erroMoradia').textContent = "Selecione uma moradia";
-    valido = false;
-    } else {
-        document.getElementById('erroMoradia').textContent = "";
-    }
-    if(moradia.value === "ap"){
+
+    if (moradia && moradia.value === "ap") {
         permiteAnimais.style.display = "block";
-        if(permite.value === "permite"){
+        possuirQuintal.style.display = "none";
+        if (permite.value === "permite") {
             permiteAnimais.style.backgroundColor = "white";
-        } else {
-            document.getElementById('erroPermite').textContent = "Você não pode ter animais";
-            permiteAnimais.style.backgroundColor = "rgb(255, 184, 157)";
-            valido = false;
-        }
+        }else{
+                document.getElementById('erroPermite').textContent = "Você não pode ter animais";
+                permiteAnimais.style.backgroundColor = "rgb(255, 184, 157)";
+                valido = false;
+             }
     }
 
-    if(valido){
+
+    //--------------------------------validação do botão submit--------------------------------------//
+
+    if (valido) {
         alert("Formulário enviado com sucesso!");
         form.submit();
     }
